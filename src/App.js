780269208d71f4d1dd3e8344641e0e71c8fdbc44
hamza <hayanshyk@gmail.com>
2021-09-23 02:07:06 +0500

    Working on hero

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
-  return <>Hellpo</>;
+  return (
+    <>
+      <Home />
+    </>
+  );
 }
 
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,28 @@ import styles from "./home.module.css";
 const Home = () => {
   return (
     <div className={styles.main}>
-      <section className={styles.hero_wrapper}></section>
+      <div className={styles.container}>
+        <div className={styles.hero}>
+          <section className={styles.hero_wrapper}>
+            <div className="row">
+              <div className="col-12 col-lg-6 col-md-6 col-sm-12">
+                <h4>Clothes collection with Premium Quality</h4>
+                <h2>Simple Way to choose your clothes</h2>
+                <form>
+                  <input
+                    type="text"
+                    name=""
+                    id=""
+                    placeholder="Enter your email to get discount 20%"
+                  />
+                  <input type="submit"></input>
+                </form>
+              </div>
+              <div className="col-12 col-lg-6 col-md-6 col-sm-12"></div>
+            </div>
+          </section>
+        </div>
+      </div>
     </div>
   );
 };
