b551c1c12445d77588c304d682201faf4de4285d
NaqashAhmed471 <naqashahmed471@gmail.com>
2021-09-23 15:46:45 +0500

    :(

--- src/pages/Home/Index.js
+++ src/pages/Home/Index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import styles from "./home.module.css";
-import Card from "../../components/Card/Index";
+// import Card from "../../components/Card/Index";
 
 const Home = () => {
   return (
     <>
-      <Card />
+      {/* <Card /> */}
     </>
   );
 };
