b09aea07c642bad118065cc8579f17bb8dfff858
Abdumuneeb <muneebafridi07@gmail.com>
2021-09-23 16:06:42 +0500

    Revert "<->"
    This reverts commit d795bbb6e2d15779d28b0435023e3b66df71c9a5.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/Home";
+import Home from "./pages/Home/Index";
 import Category from "./pages/Catagory";
 
 function App() {
