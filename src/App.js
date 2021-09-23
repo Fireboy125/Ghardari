7c0a34acd24ae6fd76d3bc86b1f48ce7663ec2b6
hamza <hayanshyk@gmail.com>
2021-09-23 10:37:56 +0500

    Erros removed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Switch, Route } from "react-router";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home/Index";
 import Category from "./pages/Catagory";
@@ -6,11 +6,13 @@ import Category from "./pages/Catagory";
 function App() {
   return (
     <div>
-      <Switch>
-        {/* <Route exact path="/" component={Category} /> */}
-        <Route exact path="/category:category" component={Category} />
-        <Route exact path="/home" component={Home} />
-      </Switch>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Category} />
+          <Route exact path="/category:category" component={Category} />
+          <Route exact path="/home" component={Home} />
+        </Switch>
+      </Router>
     </div>
   );
 }
