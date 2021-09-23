f18e56b244f1c4392bfe26839cb1f22a615c5e87
Abdumuneeb <muneebafridi07@gmail.com>
2021-09-23 16:46:14 +0500

    ok

--- src/pages/Home/Index.js
+++ src/pages/Home/Index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./home.module.css";
-import Card from "../../components/Card/Index";
+import Card from "../../components/Card";
 
 const Home = () => {
   return (
