420e2f86656b2e567b4d7230918ee79420db6a40
NaqashAhmed471 <naqashahmed471@gmail.com>
2021-09-23 15:03:08 +0500

    login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React from "react";
 import "./App.css";
+import Login from "./pages/loginPage/Login";
 
-function App() {
-  return <>Hamza</>;
-}
+const App = () => {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+};
 
 export default App;
