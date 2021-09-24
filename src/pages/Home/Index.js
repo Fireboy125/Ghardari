ab457f44d2564b941b3c4110fda5eed690f9b72a
Abdumuneeb <muneebafridi07@gmail.com>
2021-09-24 10:40:32 +0500

    Card Updated and product Detail

---
+++ src/pages/Home/Index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./home.module.css";
+import Card from "../../components/Card/Index";
+
+const Home = () => {
+  return (
+    <>
+      <Card />
+    </>
+  );
+};
+
+export default Home;
